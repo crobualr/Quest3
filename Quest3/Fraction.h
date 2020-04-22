e61126ecb0cee0098a9c60cfba977ec9445d851f
crobualr <60245615+crobualr@users.noreply.github.com>
2020-04-21 23:22:30 -0500

    Update Fraction.h
    Added arithmetic operator overloaders

--- Quest3/Fraction.h
+++ Quest3/Fraction.h
@@ -6,6 +6,10 @@ public:
 	Fraction(int numerator1, int denominator1);
 	int GCF(int numerator1, int denominator1);
 	void reducedFraction(int numerator1, int denominator1);
+	friend void operator+(Fraction one, Fraction two);
+	friend void operator-(Fraction one, Fraction two);
+	friend void operator*(Fraction one, Fraction two);
+	friend void operator/(Fraction one, Fraction two);
 	int numerator;
 	int denominator;
 };
