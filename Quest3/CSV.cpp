4d3bf5bfeeedf2b69fae54351bdae2ca8be7e9cb
crobualr <60245615+crobualr@users.noreply.github.com>
2020-04-21 22:50:19 -0500

    Update CSV.cpp
    Modified CSv file to call the fractions class constructor and methods.

--- Quest3/CSV.cpp
+++ Quest3/CSV.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include "CSV.h"
+#include "Fraction.h"
 using namespace std;
 
 //CSV::CSV() {};
@@ -13,11 +14,20 @@ CSV::CSV(string csvFile) {
 	}
 	while (fin.good()) {
 		string num, num2, den, den2, operation;
+		int numeratorFraction, denominatorFraction;
 		getline(fin, num, ',');
 		getline(fin, den, ',');
+		numeratorFraction = stoi(num);
+		denominatorFraction = stoi(den);
+		Fraction fraction1(numeratorFraction, denominatorFraction);
+		fraction1.reducedFraction(numeratorFraction, denominatorFraction);
 		getline(fin, operation, ',');
 		getline(fin, num2, ',');
 		getline(fin, den2, '\n');
+		numeratorFraction = stoi(num2);
+		denominatorFraction = stoi(den2);
+		Fraction fraction2(numeratorFraction, denominatorFraction);
+		fraction2.reducedFraction(numeratorFraction, denominatorFraction);
 		cout << num << "/" << den << operation << num2 << "/" << den2 << endl;
 	}
 	fin.close();
